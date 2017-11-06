7953e24e394e9426c9ff46941935b286ec81306e
gerardus ari adiprana <gerardus.adiprana@metraplasa.co.id>
2017-11-06 15:40:36 +0700

    2

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import express from 'express'
+import consign from 'consign'
 
-const PORT = 3000
 const app = express()
 
-app.set("json spaces", 4)
-
-app.get("/", (req, res) => res.json({status : "API"}))
-app.get("/users", (req,res) => {
-  res.json({
-    tasks: [
-      {title: "test1"},
-      {title: "test2"}
-    ]
-  })
-})
-
-app.listen(PORT, () => console.log(`API - Port ${PORT}`))
+consign()
+  .include("models")
+  .then("libs/middlewares.js")
+  .then("routes")
+  .then("libs/boot.js")
+  .into(app)
