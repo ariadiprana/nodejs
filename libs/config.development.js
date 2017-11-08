0db2dbd6178b2468ad8f717129762d026a4440be
gerardus ari adiprana <gerardus.adiprana@metraplasa.co.id>
2017-11-08 16:13:19 +0700

    finish API basic

--- libs/config.development.js
+++ libs/config.development.js
@@ -1,8 +1,15 @@
+import logger from "./logger.js";
+
 module.exports = {
   host: "localhost",
   database: "trave",
   username: "root",
-  password: "",
-  jwtSecret: "",
-  jwtSession: {session: false}
+  password: "Ad1pr4n4",
+  jwtSecret: "Ad1pr4n4",
+  jwtSession: {session: false},
+  params: {
+    logging: (sql) => {
+      logger.info(`[${new Date()}] ${sql}`);
+    }
+  }
 }
